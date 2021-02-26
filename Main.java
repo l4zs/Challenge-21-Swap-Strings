00f664911578ca201f13c04a398d83b47c4ec786
l4zs <68546083+l4zs@users.noreply.github.com>
2021-02-26 17:38:35 +0100

    Update Main.java

--- Main.java
+++ Main.java
@@ -13,10 +13,10 @@ public class Main{
         // Example: b = "World!";
         String b = myObj.nextLine();  // Read user input and save it to b
 
-        System.out.println("Before swap:\na: " + a + "\nb: " + b + "\n"); // Show that values are assigned correctly
+        System.out.println("Before swap:\na: " + a + "\nb: " + b + "\n"); // Show that values are assigned correctly | Example Output: "Before swap: a: Hello b: World!"
         a = a + b; // add b to a | Example: a = "HelloWorld!";
         b = a.substring(0, a.length() - b.length()); // set b to original a by setting it to a while removing b from a set before | Example: b = "HelloWorld!" - "World!"; -> b = "Hello";
         a = a.substring(b.length()); // set a to original b by removing new b (original a) from new a (right now original a + original b) | Example: a = "HelloWorld!" - "Hello"; -> a = "World!";
-        System.out.println("After swap:\na: " + a + "\nb: " + b + "\n"); // Show that values are swapped correctly
+        System.out.println("After swap:\na: " + a + "\nb: " + b + "\n"); // Show that values are swapped correctly | Example Output: "After Swap: a: World! b: Hello"
     }
 }
